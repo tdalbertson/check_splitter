604eb8d044aa8a02a07716d68f75c9644a607718
Tim Albertson <tdalbertson@gmail.com>
2020-10-09 00:30:22 -0700

    completed initial program

--- check_splitter.py
+++ check_splitter.py
@@ -9,6 +9,28 @@ import os
 def split_check(total, number_of_people):
     return math.ceil(total / number_of_people)
 
+def get_check_total():
+    while True:
+        try:
+            total_of_check = float(input("What is the total of the check? $"))
+            break
+        except ValueError:
+            clear_screen()
+            print("\nPlease enter a valid check total.\n")
+
+    return total_of_check
+
+def get_num_of_people():
+    while True:
+        try:
+            total_people = int(input("How many people? "))
+            break
+        except ValueError:
+            clear_screen()
+            print("\nPlease enter a valid number of people.\n")
+
+    return total_people
+
 def clear_screen():
     os.system('clear')
 
@@ -16,11 +38,9 @@ def clear_screen():
 clear_screen()
 print("This program splits a check depending on the number of people.\n")
 
-# Input Validation
-while True:
-    try:
-        total_of_check = float(input("What is the total of the check? "))
-        break
-    except ValueError:
-        clear_screen()
-        print("\nPlease enter a valid check total.\n")
+total_of_check = get_check_total()
+total_persons = get_num_of_people()
+cost_per_person = split_check(total_of_check, total_persons)
+
+print(f"Total:    {total_of_check}\n# of Persons:   {total_persons}\nCost per person:   ${cost_per_person}")
+
