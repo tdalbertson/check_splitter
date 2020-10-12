2327767c8644f70e34b3bacab40896042a58169f
Tim Albertson <tdalbertson@gmail.com>
2020-10-11 22:17:13 -0700

    update check total input validation and string formatting

--- check_splitter.py
+++ check_splitter.py
@@ -3,9 +3,9 @@ A program for splitting a check between multiple people.
 """
 
 import math
-import os
+import os # For clearing terminal screen
 
-# Function Declarations
+# Function Declarations ==================================================
 def split_check(total, number_of_people):
     return math.ceil(total / number_of_people)
 
@@ -13,7 +13,11 @@ def get_check_total():
     while True:
         try:
             total_of_check = float(input("What is the total of the check? $"))
-            break
+            if total_of_check <= 0:
+                clear_screen()
+                print("\nPlease enter a valid check total.\n")
+            else:
+                break
         except ValueError:
             clear_screen()
             print("\nPlease enter a valid check total.\n")
@@ -21,6 +25,7 @@ def get_check_total():
     return total_of_check
 
 def get_num_of_people():
+    # Input validation
     while True:
         try:
             total_people = int(input("How many people? "))
@@ -42,7 +47,7 @@ def get_num_of_people():
 def clear_screen():
     os.system('clear')
 
-# Main Program Flow
+# Main Program Flow ==================================================
 clear_screen()
 print("This program splits a check depending on the number of people.\n")
 
@@ -50,5 +55,5 @@ total_of_check = get_check_total()
 total_persons = get_num_of_people()
 cost_per_person = split_check(total_of_check, total_persons)
 
-print(f"Total:    {total_of_check}\n# of Persons:   {total_persons}\nCost per person:   ${cost_per_person}")
+print(f"Total:    {total_of_check:.2f}\n# of Persons:   {total_persons}\nCost per person:   ${cost_per_person}")
 
